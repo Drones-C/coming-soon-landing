64edd492afd461cc09f8c5ef496bfa1644fc5f1e
Sasha <lonagi22@gmail.com>
2020-11-11 01:40:57 +0200

    cache

--- php/configs/consts.php
+++ php/configs/consts.php
@@ -4,7 +4,7 @@ $_DOMAIN = "https://".$DOMAIN;
 $SITE_TITLE = 'DronesC';
 $DESCRIPTION = $__translations['d0'];
 $KEYWORDS = 'NVG, Drones, Drones Control, DronesC, Construction, Order, Service, services, flights, modules, analysis, landing';
-$SN_BANNER = "images/sn_share.png";
+$SN_BANNER = "images/sn_share.png?0";
 $MAIN_COLOR = "#131313";
 $CACHE = "3";
 
