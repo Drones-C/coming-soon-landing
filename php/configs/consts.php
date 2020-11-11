8d593efca7b7552bd842465e08a68b1c7ae2975f
Sasha <40319171+lonagi@users.noreply.github.com>
2020-11-11 13:36:26 +0200

    change theme

--- php/configs/consts.php
+++ php/configs/consts.php
@@ -4,8 +4,8 @@ $_DOMAIN = "https://".$DOMAIN;
 $SITE_TITLE = 'DronesC';
 $DESCRIPTION = $__translations['d0'];
 $KEYWORDS = 'NVG, Drones, Drones Control, DronesC, Construction, Order, Service, services, flights, modules, analysis, landing';
-$SN_BANNER = "images/sn_share.png?0";
-$MAIN_COLOR = "#131313";
-$CACHE = "4";
+$SN_BANNER = "images/sn_share.png?1";
+$MAIN_COLOR = "#FCD532";
+$CACHE = "6";
 
 ?>
