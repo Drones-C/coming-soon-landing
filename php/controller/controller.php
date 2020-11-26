0b8d45e1f8348e811d0077aab45b4c23230ad4b5
Sasha <lonagi22@gmail.com>
2020-11-27 01:01:22 +0200

    func only for host

--- php/controller/controller.php
+++ php/controller/controller.php
@@ -6,8 +6,10 @@ if(in_array($lang, $langs))
 include_once "php/configs/consts.php";
 
 //Stat
-$cccc = new nvgData("stati","ips",'ip',"dronesc",6,1,1);
-$cccc2 = new nvgCount("stati","visitors","dronesc");
-$cccc2->enableLangC("langs");
-$cccc2->Count();
+if($_SERVER['HTTP_HOST']!="localhost") {
+    $cccc = new nvgData("stati","ips",'ip',"dronesc",6,1,1);
+    $cccc2 = new nvgCount("stati","visitors","dronesc");
+    $cccc2->enableLangC("langs");
+    $cccc2->Count();
+}
 ?>
