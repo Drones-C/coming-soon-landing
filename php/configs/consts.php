4a2b3c38f35f6ef4b3cfa12443332b3342a786c5
Sasha <lonagi22@gmail.com>
2020-11-27 01:01:07 +0200

    Update consts.php

--- php/configs/consts.php
+++ php/configs/consts.php
@@ -6,6 +6,8 @@ $DESCRIPTION = $__translations['d0'];
 $KEYWORDS = 'NVG, Drones, Drones Control, DronesC, Construction, Order, Service, services, flights, modules, analysis, landing';
 $SN_BANNER = "images/sn_share.png?1";
 $MAIN_COLOR = "#FCD532";
-$CACHE = "6";
+$CACHE = "7";
+
+$G_VERSION = "0.0.1";
 
 ?>
