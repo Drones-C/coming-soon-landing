6bcbb013209287b4de66a1dd3c32b2cb7d465843
Sasha <lonagi22@gmail.com>
2020-11-27 01:01:13 +0200

    new words

--- php/configs/en.php
+++ php/configs/en.php
@@ -3,7 +3,11 @@
 $__translations = [
 	"cc"=>"Change language",
 	"d0"=>"DronesC is a company that creates software for modeling and designing drones in a 3D interactive platform. Using our 3D platform, you can not only create individual layouts, but also program and test drones in various scenarios and conditions. If you already want to try to analyze any drones, order layouts of boards or drones, then you can find here",
-	"pp"=>"more"
+	"pp"=>"more",
+    "pp2"=>"You can try the DEMO version",
+    "here"=>"here",
+    "download"=>"Download",
+    "for"=>"for"
 ];
 
 ?>
--- php/configs/ro.php
+++ php/configs/ro.php
@@ -3,7 +3,11 @@
 $__translations = [
     "cc"=>"Schimbă limba",
     "d0"=>"DronesC este o companie care construiește software-ul de modelare și proiectare a dronelor într-o platformă 3D interactivă. Folosind designerul nostru 3D, este posibil nu numai să creați machete personalizate, ci și să programați și să testați dronii în diferite scenarii și сondiții. Dacă doriți deja să încercați să analizați orice drone, să comandați machete de plăci sau dronuri, atunci puteți vedea aici",
-    "pp" =>"mai mult"
+    "pp" =>"mai mult",
+    "pp2"=>"puteți încerca versiunea DEMO",
+    "here"=>"aici",
+    "download"=>"Descarca",
+    "for"=>"pentru"
 ];
 
 ?>
